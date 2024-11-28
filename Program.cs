05c7a67782cf44afa2a36f99117262b4939cef3b
Loredana Penea <Nicoleta-Loredana.Penea@endava.com>
2024-11-28 16:04:50 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello!");
+
+Console.WriteLine("Please type in your name:");
+string name = Console.ReadLine();
+Console.WriteLine("Please type in your age:");
+var age = Console.ReadLine();
+Console.WriteLine("Your name is: " + name + " and you are " + age + " years old");
+Console.WriteLine($"Your name is {name} and you have {age} years");
+int age20 = int.Parse(age) + 20;
+Console.WriteLine($"You will be {age20} years old in 20 years");
