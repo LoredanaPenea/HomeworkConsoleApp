330c8f64dcfbb37b8cc4932e2dbdd7b75720c5f0
Loredana Penea <Nicoleta-Loredana.Penea@endava.com>
2024-12-16 20:26:52 +0200

    Rename class

---
+++ Homework1/Cursant.cs
@@ -0,0 +1,40 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Reflection;
+using TestConsoleApp.Homework2;
+
+
+Console.WriteLine("Hello!");
+
+//Console.WriteLine("Please type in your name:");
+//string name = Console.ReadLine();
+//Console.WriteLine("Please type in your age:");
+//var age = Console.ReadLine();
+//Console.WriteLine($"Your name is {name} and you have {age} years");
+//int age20 = int.Parse(age) + 20;
+//Console.WriteLine($"You will be {age20} years old in 20 years");
+
+Person person1 = new Person();
+Console.WriteLine("Type in your first name:");
+person1.firstName = Console.ReadLine();
+//Console.WriteLine($"Your first name is {person1.firstName}");
+
+Console.WriteLine("Type in your last name:");
+person1.lastName = Console.ReadLine();
+//Console.WriteLine($"Your last name is {person1.lastName}");
+
+Console.WriteLine("Please type in your age:");
+var yourAge = Console.ReadLine();
+person1.age = int.Parse(yourAge);
+
+Console.WriteLine("Please type in your gender:");
+person1.gender = Console.ReadLine();
+
+Console.WriteLine($"Person details are as follows: \n" +
+        $"- First Name: {person1.firstName} \n" +
+        $"- Last Name: {person1.lastName} \n" +
+        $"- Age: {person1.age} \n" +
+        $"- Gender: {person1.gender}");
+
+person1.IsOld();
+person1.ShowGender();
+       
---
+++ Homework2/Person.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestConsoleApp.Homework2
+{
+    public class Person
+    { 
+        public string firstName;
+        public string lastName;    
+        public int age;
+        public string gender;
+
+        public void IsOld()
+        {
+            if (age >= 100)
+            {
+                Console.WriteLine("You are ancient!");
+            }
+            else if (age >= 50)
+            {
+                Console.WriteLine("You are old!");
+            }
+            else Console.WriteLine("You are still young!");
+        }
+
+        public void ShowGender()
+        {
+            if (gender.Equals("Male") || gender.Equals("male"))
+            {
+                Console.WriteLine("You are a man!");
+            }
+            else if (gender.Equals("Female") || gender.Equals("female") )
+                Console.WriteLine("You are a woman!");
+            else Console.WriteLine("You haven't specified you gender!");
+        }
+
+    }
+
+}
